d301ef2fb077ddc6d7bdbf306ac8d48204a9fbbf
the_Aristo <gogin04@mail.ru>
2023-09-12 15:32:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 const ConfigOptions = {
     port: process.env.PORT,
-    key: fs.readFileSync("/Users/velimirhlebnikov/Tests/self/certs/example.key"),
-    cert: fs.readFileSync("/Users/velimirhlebnikov/Tests/self/certs/example.crt"),
+    key: fs.readFileSync("./certs/example.key"),
+    cert: fs.readFileSync("./certs/example.crt"),
     app_id: process.env.APP_ID,
     app_key: process.env.APP_KEY,
 }
